c9ff666e79f8958d149bd38bd1bf7c0ab721b1b2
alexgalicio <alexisjoygalicio@gmail.com>
2022-07-19 16:52:19 +0800

    Control Flow and Logical Operators

--- Day 3/day-3-6-project-chooose-your-own-adventure-game.py
+++ Day 3/day-3-6-project-chooose-your-own-adventure-game.py
@@ -1,14 +1,20 @@
-print('Welcome to Treasure Island. Your mission is to find the treasure.')
-way = input('There is a two way. Choose between left(l) or right(r)')
-if way == "r":
-    swim = input('Swim(s) or wait(w) ')
-    if swim == 'w':
-        door = input('There are two door: Red(r) or blue(b) or Pink(p) ')
-        if door == 'p':
-            print('You win!')
+print('Welcome to Treasure Island.\nYour mission is to find the treasure.')
+direction = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right' ")
+if direction == 'right':
+    next = input("You've come to the lake. There is an island in the middle of the lake. "
+                 "Type 'wait' to wait for a boat. Type 'swim' to swim across'")
+    if next == 'wait':
+        stage3 = input("You arrive at the island unharmed. There is a house with 3 doors. "
+                       "One red, one yellow and one blue. Which colour do you choose?")
+        if stage3 == 'yellow':
+            print("You found the treasure! You Win!")
+        elif stage3 == 'red':
+            print("It's a room full of fire. Game Over.")
+        elif stage3 == 'blue':
+            print("You enter a room of beasts. Game Over.")
         else:
-            print("It's morbin time!")
+            print("You chose a door that doesn't exist. Game Over.")
     else:
-        print("It's morbin time!")
+        print("You get attacked by an angry trout. Game Over.")
 else:
-    print("It's morbin time!")
+    print('You fell into a hole. Game Over.')
