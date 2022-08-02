448ee0e85e9e3bb15ec50c7b40f1677642d21cb7
alexgalicio <alexisjoygalicio@gmail.com>
2022-08-02 15:59:01 +0800

    The Quiz Project & the Benefits of OOP

---
+++ Intermediate/Day 17/quiz_game.py
@@ -0,0 +1,19 @@
+from quiz_question_model import Question
+from quiz_game_data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    q_text = question['question']
+    q_answer = question['correct_answer']
+    new_question = Question(q_text, q_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz.")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
---
+++ Intermediate/Day 17/quiz_game_data.py
@@ -0,0 +1,82 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "All program codes have to be compiled into an executable file in order to be run."
+                    " This file can then be executed on any machine.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "To bypass US Munitions Export Laws,"
+                    " the creator of the PGP published all the source code in book form.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "MacOS is based on Linux.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The open source program Redis is a relational database server.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMD created the first consumer 64-bit processor.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first dual-core CPU was the Intel Pentium D.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Time on Computers is measured via the EPOX System.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
